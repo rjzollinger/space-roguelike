ac6234935681686ba9d48ed0d0878b1e3945af1a
zhenbang <zbchen29@gmail.com>
2020-02-29 00:06:47 -0500

    Add basic health bar UI panel

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    [Header("Heath Elements")]
+    public Text healthText;
+    public Slider healthSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Set the UI health bar to health
+    public void SetUIHealth(int health)
+    {
+        healthText.text = health.ToString();
+        healthSlider.value = (float)health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
