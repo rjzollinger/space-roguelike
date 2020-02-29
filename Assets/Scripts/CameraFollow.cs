ef26f1925d36b5f837da8804a73614ec55ae2f45
Preston Bezos <pbezos@mit.edu>
2020-02-28 23:27:56 -0500

    added camera follow script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public Vector3 offset;
+    private Vector3 velocity = Vector3.zero;
+
+    private void FixedUpdate()
+    {
+        Vector3 desired = target.position + offset;
+        Vector3 smoothed = Vector3.SmoothDamp(transform.position, desired, ref velocity, smoothing);
+        transform.position = smoothed;
+        transform.LookAt(new Vector3(transform.position.x, target.position.y, target.position.z));
+    }
+}
