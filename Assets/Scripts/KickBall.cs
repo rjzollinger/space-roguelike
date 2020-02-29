989be316e5c70f12dbf59cf5ab49d751fd919212
rjzollinger <61205332+rjzollinger@users.noreply.github.com>
2020-02-29 15:39:18 -0500

    Many balls can be spawned

--- Assets/Scripts/KickBall.cs
+++ Assets/Scripts/KickBall.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class KickBall : MonoBehaviour
 {
     public GameObject player;
-    public GameObject ballProjectile;
+    public Rigidbody ballProjectile;
+    private Vector3 spawnOffset = new Vector3(1,0,0);
+    public float ballSpeed = 5;
+    private int existingBalls = 0;
 
         // Start is called before the first frame update
         void Start()
@@ -16,8 +19,12 @@ public class KickBall : MonoBehaviour
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetKeyDown("space")) {
-                Instantiate(ballProjectile, player.transform.position, Quaternion.identity);
+            if (Input.GetKeyDown("space") && existingBalls < 30) {
+                Rigidbody ball = Instantiate(ballProjectile, player.transform.position + spawnOffset, Quaternion.identity);
+                ball.velocity = transform.forward * ballSpeed;
+                existingBalls++;
             }
         }
+
+        //void OnCollision
 }
