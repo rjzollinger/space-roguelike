c6eab144d31ee071d8429d53af417eb0a97fdcd3
Zhenjia Chen <zhenjia@mit.edu>
2020-03-02 16:15:39 -0500

    Enemies shoot projectiles when in range of player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [Header("Public Reference")]
+    public Rigidbody projectile;
+
+    [Header("Movement/Turn Speed")]
     public float moveScale;
     public float rotationScale;
+
+    [Header("Attributes/Behavior")]
+    public int health;
     public float aggroDistance;
     public float followDistance;
+    public float projectileVelocity;
+    public float shootInterval;
+
     private Transform player;
+    private float fireTimer;
 
     // Start is called before the first frame update
     void Start()
@@ -17,23 +28,35 @@ public class Enemy : MonoBehaviour
     }
 
     void Aggro(float dist) {
-        Vector3 desired = player.position;
+        fireTimer += Time.deltaTime;
         Vector3 lookDirection = player.position - transform.position;
-
+        Quaternion rotation = Quaternion.LookRotation(lookDirection);
+    
         transform.rotation = Quaternion.Slerp(
             transform.rotation,
-            Quaternion.LookRotation(lookDirection),
+            rotation,
             rotationScale
         );
+
+        if (fireTimer > shootInterval) {
+            FireProjectile(player.position, rotation);
+            fireTimer = 0;
+        }
 
         if (dist > followDistance) {
             transform.position = Vector3.MoveTowards(
                 transform.position,
-                desired,
+                player.position,
                 moveScale
             );
         }
     }
+
+    void FireProjectile(Vector3 target, Quaternion rotation) {
+        Rigidbody newProjectile = Instantiate(projectile, transform.position, rotation);
+        newProjectile.velocity = (target - transform.position).normalized * projectileVelocity;
+    }
+
     void Idle() {
         transform.Rotate(0,1,0);
     }
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float lifespan;
+    void Start()
+    {
+        Destroy(gameObject, lifespan);
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        GameObject target = collider.gameObject;
+        if (collider.gameObject.tag == "Player") {
+            Debug.Log("Player hit");
+            Destroy(gameObject);
+        }
+    }
+}
