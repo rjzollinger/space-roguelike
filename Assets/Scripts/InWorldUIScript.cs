a7c8fef132f272c2cf0549079e35baa8aca03361
zhenbang <zbchen29@gmail.com>
2020-03-09 18:42:31 -0400

    Implement basic world UI script

---
+++ Assets/Scripts/InWorldUIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InWorldUIScript : MonoBehaviour
+{
+    // The target transform for this UI element to follow
+    public Transform target;
+    // The canvas object to place this UI element into
+    // private Transform canvas;
+    // The camera to use for projection
+    public Camera mainCamera;
+
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mainCamera.WorldToScreenPoint(target.position) + offset;
+    }
+}
